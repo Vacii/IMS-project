af68f60206819d9feff2fc2857573d92e725c857
Vacii <lv.vaclavek@gmail.com>
2022-11-25 19:54:12 +0100

    incoming order is handeled

---
+++ Makefile
@@ -0,0 +1,15 @@
+# author: Pavol Babjak - xbabja03; Lukáš Václavek - xvacla32
+# Projekt #13. SHO v logistice
+
+ZIP_NAME = 13_xbabja03_xvacla32.zip
+
+all:
+	g++ -Wall -pedantic -std=c++11 main.cpp -o ims -lsimlib -lm
+run:
+	./ims
+
+clean:
+	rm -f ims *.o $(ZIP_NAME)
+
+zip:
+	zip -r $(ZIP_NAME) main.cpp Makefile Technicka_sprava.pdf README.md
--- main.cpp
+++ main.cpp
@@ -1,5 +1,86 @@
+/**
+ * @file main.cpp
+ *
+ * @brief Simulation of Tesco grocery delivery service
+ *
+ * @author Pavol Babjak - xbabja03
+ * @author Lukáš Václavek - xvacla32
+ *
+ */
+
 #include <string>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h> 
 #include <simlib.h>
+
+//Time is in minutes
+
+#define NUMOFPICKERS 10
+#define NUMOFWAREHOUSEMEN 1
+#define NUMOFCARS 1
+
+Store  Picking("Picking", NUMOFPICKERS);
+Store WarehouseWork("Warehouse", NUMOFWAREHOUSEMEN);
+Facility Car("Car");
+Histogram Table("Table",0,25,20);
+
+//order handler
+class Order : public Process{ 
+  double Prichod;
+  void Behavior(){
+    Prichod = Time;
+    Enter(Picking);
+    Wait(10);
+    Leave(Picking, 1);
+
+    Enter(WarehouseWork);
+    Wait(Exponential(3));
+    Leave(WarehouseWork, 1);
+
+
+    Table(Time-Prichod);          //waiting and service time
+  }
+};
+
+//Nevim jak implementovat generovani ridice a predani objednavky k nemu
+class Driver : public Process{
+    double Prichod;
+    void Behavior(){
+        Prichod = Time;
+        Seize(Car);
+
+
+
+
+    }
+    void LoadOrder(){
+
+    }
+};
+
+//input generator
+class OrderGenerator : public Event{
+  void Behavior(){
+    (new Order)->Activate();        //new order
+    Activate(Time+Exponential(1));  //every minute
+  }
+};
+
+class DriverGenerator : public Event{
+  void Behavior(){
+    (new Driver)->Activate();        //new driver
+    Activate(Time+Exponential(240)); //every 4h
+  }
+};
+
+int main(){
+  Init(0,960);                          //time limit is set for 16hours (one day)
+  (new OrderGenerator)->Activate();     //order generator
+  (new DriverGenerator)->Activate();    //driver generator
+  Run();                                //run simulation
+  Picking.Output();                     //print of results
+  WarehouseWork.Output();
+  Table.Output();
+  return 0;
+}
