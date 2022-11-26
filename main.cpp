a12ab49ac19ca844bb63b49da4b095eb9ffef7cb
Vacii <lv.vaclavek@gmail.com>
2022-11-26 19:22:09 +0100

    delivery is somewhat connected

--- main.cpp
+++ main.cpp
@@ -16,14 +16,48 @@
 
 //Time is in minutes
 
+
+//TODO Nemáme moc domyšlené objednávky, ale určitě ne všechny mají být na aktuální den
+//TODO beru jen jeden nákup k rozvozu, potřeba implementovat jaký počet nákupů se bude brát
+
 #define NUMOFPICKERS 10
 #define NUMOFWAREHOUSEMEN 1
 #define NUMOFCARS 1
 
 Store  Picking("Picking", NUMOFPICKERS);
 Store WarehouseWork("Warehouse", NUMOFWAREHOUSEMEN);
-Facility Car("Car");
+Store Car("Car", NUMOFCARS);
+Facility LoadingCar("Loading order");
+Facility Delivery("Delivering order");
+
+
 Histogram Table("Table",0,25,20);
+
+Queue driverWithCar;
+Queue orderPacked;
+
+class DeliveryProcess : public Process{
+  double Prichod;
+  void Behavior(){
+    Seize(Delivery);
+    Wait(Exponential(180));
+    Release(Delivery);
+  }
+};
+
+class LoadOrder : public Process{
+  double Prichod;
+  void Behavior(){
+    driverWithCar.GetFirst()->Activate();
+    Seize(LoadingCar);
+    Wait(8);
+    Release(LoadingCar);
+    Leave(Car, 1);
+    (new DeliveryProcess)->Activate();
+  }
+};
+
+
 
 //order handler
 class Order : public Process{ 
@@ -33,28 +67,33 @@ class Order : public Process{
     Enter(Picking);
     Wait(10);
     Leave(Picking, 1);
-
     Enter(WarehouseWork);
     Wait(Exponential(3));
     Leave(WarehouseWork, 1);
 
+    if(driverWithCar.Length() > 0){
+      // driverWithCar.GetFirst()->Activate();
+      orderPacked.GetFirst()->Activate();
+      (new LoadOrder)->Activate();
+    } else {
+      Into(orderPacked);
+      Passivate();
+    }
+    
 
     Table(Time-Prichod);          //waiting and service time
   }
 };
 
-//Nevim jak implementovat generovani ridice a predani objednavky k nemu
 class Driver : public Process{
     double Prichod;
     void Behavior(){
         Prichod = Time;
-        Seize(Car);
+        Enter(Car);
 
+        Into(driverWithCar);
+        Passivate();
 
-
-
-    }
-    void LoadOrder(){
 
     }
 };
@@ -63,24 +102,27 @@ class Driver : public Process{
 class OrderGenerator : public Event{
   void Behavior(){
     (new Order)->Activate();        //new order
-    Activate(Time+Exponential(1));  //every minute
+    Activate(Time+Exponential(5));  //new order comes every 5 mins exp
   }
 };
 
 class DriverGenerator : public Event{
   void Behavior(){
     (new Driver)->Activate();        //new driver
-    Activate(Time+Exponential(240)); //every 4h
+    Activate(Time+240);              //every 4h
   }
 };
 
 int main(){
   Init(0,960);                          //time limit is set for 16hours (one day)
   (new OrderGenerator)->Activate();     //order generator
-  (new DriverGenerator)->Activate();    //driver generator
+  (new DriverGenerator)->Activate(120);    //driver generator
   Run();                                //run simulation
   Picking.Output();                     //print of results
   WarehouseWork.Output();
+  Car.Output();
+  LoadingCar.Output();
+  Delivery.Output();
   Table.Output();
   return 0;
 }
