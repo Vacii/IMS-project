d78b525503f51230fbf033996662ea73358758e7
xbabja03 <63399111+xbabja03@users.noreply.github.com>
2022-11-30 02:30:36 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -17,37 +17,30 @@
 
 //Time is in minutes
 
-
-//TODO Nemáme moc domyšlené objednávky, ale určitě ne všechny mají být na aktuální den
-//TODO beru jen jeden nákup k rozvozu, potřeba implementovat jaký počet nákupů se bude brát
-
 #define NUMOFPICKERS 10
 #define NUMOFWAREHOUSEMEN 1
 #define NUMOFCARS 4
 
+int random_gen(int min,int max)
+{
+  std::random_device rd; // obtain a random number from hardware
+  std::mt19937 gen(rd()); // seed the generator
+  std::uniform_int_distribution<> distr(min, max); // define the range
+  return(distr(gen));
+}
+
 int finalized_orders = 0;
 int shifts = 3;
 
-
-//int remaining_orders = Random(10,30);
-
-  std::random_device rd; // obtain a random number from hardware
-  std::mt19937 gen(rd()); // seed the generator
-  std::uniform_int_distribution<> distr(10, 30); // define the range
-  //std::cout << distr(gen) << ' '; // generate numbers
-
-int remaining_orders = distr(gen);
-
+int remaining_orders = random_gen(10,30);
 bool opened = false;
 
 Store Picking("Picking", NUMOFPICKERS);
 Store WarehouseWork("Warehouse", NUMOFWAREHOUSEMEN);
 Store Car("Car", NUMOFCARS);
-//Facility LoadingCar("Loading order");
-//Facility Delivery("Delivering order");
 
-
-Histogram Table("Table",0,3,20);
+Histogram Table("Príprava objednávky až po expedíciu",0,3,20);
+Histogram Table2("Nakládka tovar kuriérovi",0,3,20);
 
 Queue driverWithCar;
 Queue orderPacked("Objednavky ready na expediciu");
@@ -55,28 +48,22 @@ Queue orderPacked("Objednavky ready na expediciu");
 
 class DeliveryProcess : public Process{
   void Behavior(){
-    //Seize(Delivery);
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 gen(rd()); // seed the generator
-    std::uniform_int_distribution<> distr(120, 240); // define the range
-    Wait(distr(gen));
+    Wait(random_gen(120,240));
     Leave(Car, 1);
-    //Release(Delivery);
   }
 };
 
 class LoadOrder : public Process{
+  double zaciatok_nakladky;
   void Behavior(){
-    //driverWithCar.GetFirst()->Activate();
-    //Seize(LoadingCar);
+    zaciatok_nakladky = Time;
     unsigned int order_to_be_taken = orderPacked.Length() / 4;
     for(unsigned int i=0;i<order_to_be_taken;i++)
     {
       (orderPacked.GetFirst())->Activate();
       Wait(5);
     }
-   
-    //Release(LoadingCar);
+    Table2(Time-zaciatok_nakladky);
 
     (new DeliveryProcess)->Activate();
   }
@@ -95,7 +82,6 @@ class LoadOrder : public Process{
 
 };*/
 
-
 //order handler
 class Order : public Process{ 
   double Prichod;
@@ -105,46 +91,18 @@ class Order : public Process{
     //if (opened)
     //{
       Enter(Picking);
-      std::random_device rd; // obtain a random number from hardware
-      std::mt19937 gen(rd()); // seed the generator
-      std::uniform_int_distribution<> distr(8, 12); // define the range
-      Wait(distr(gen));
-      Leave(Picking, 1);
+      Wait(random_gen(8,12));
+      Leave(Picking,1);
 
       Enter(WarehouseWork);
-      //std::random_device rd; // obtain a random number from hardware
-      //std::mt19937 gen(rd()); // seed the generator
-      std::uniform_int_distribution<> distr2(3, 6); // define the range
-      Wait(distr2(gen));
-      Leave(WarehouseWork, 1);
+      Wait(random_gen(3,6));
+      Leave(WarehouseWork,1);
 
       Table(Time-Prichod);
 
       Into(orderPacked);
       Passivate();
-   //}
-  
-    
-   // (new GroceryPicking)->Activate();
-   // (new CooledandFrozenPicking)->Activate();
 
-    
-
-   // finalized_orders++;
-
-   /* if(driverWithCar.Length() > 0){
-      // driverWithCar.GetFirst()->Activate();
-      orderPacked.GetFirst()->Activate();
-      (new LoadOrder)->Activate();
-    } else {
-      Into(orderPacked);
-      Passivate();
-    } */
-
-      
-    
-
-              //waiting and service time
   }
 };
 
@@ -154,10 +112,6 @@ class Driver : public Process{
         Prichod = Time;
         Enter(Car);
         (new LoadOrder)->Activate();
-        //Into(driverWithCar);
-        //Passivate();
-
-
     }
 };
 
@@ -171,23 +125,8 @@ class OrderGenerator : public Event{
       remaining_orders--;
     }
 
-    /*if (Time==0)
-    {
-      (new Order)->Activate();        //new order
-      Activate(Time+360+Exponential(5));
-      Wait(360);
-    }
-    else
-    {
-      (new Order)->Activate();        //new order
-      Activate(Time+Exponential(5));  //new order comes every 5 mins exp
-    }*/
-
     (new Order)->Activate();        //new order
       Activate(Time+Exponential(5));  //new order comes every 5 mins exp
-
-
-    
     
   }
 };
@@ -203,7 +142,6 @@ class DriverGenerator : public Event{
       }
       shifts--;
     }
-    
                 
   }
 };
@@ -221,12 +159,11 @@ class Operate : public Event{
         opened=!opened;
         Activate(Time+480);
       }
-                
+            
   }
 };
 
 int main(){
-  printf("xxx %d",remaining_orders);
   Init(0,1440);                          //time limit is set for 16hours (one day)
   (new OrderGenerator)->Activate(360);     //order generator
   (new DriverGenerator)->Activate(480);    //driver generator
@@ -234,10 +171,8 @@ int main(){
   Picking.Output();                     //print of results
   WarehouseWork.Output();
   Car.Output();
-  printf("xxxxxxxx \n");
   orderPacked.Output();
-  //LoadingCar.Output();
-  //Delivery.Output();
   Table.Output();
+  Table2.Output();
   return 0;
 }
